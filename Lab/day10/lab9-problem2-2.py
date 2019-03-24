5eacd12f0e3492d6b0f680c46638086a5bdd9482
Surattikorn Chumkaew <surattikorn@gmail.com>
2019-03-24 17:07:44 +0700

    finished day10

--- Lab/day10/lab9-problem2-2.py
+++ Lab/day10/lab9-problem2-2.py
@@ -1,5 +1,7 @@
 import numpy as np
+from numpy.linalg import svd
 from scipy.io import loadmat
+import matplotlib.pyplot as plt
 
 
 def featureNormalize(X):
@@ -7,7 +9,7 @@ def featureNormalize(X):
     Normalize the dataset X
 
     :param X:
-    :return: X_normalized, mu, sigma
+    :return:
     """
 
     mu = np.mean(X, axis=0)
@@ -16,6 +18,22 @@ def featureNormalize(X):
     X_normalized = (X - mu) / sigma
 
     return X_normalized, mu, sigma
+
+
+def pca(X):
+    """
+    Compute eigenvectors of the covariance matrix X
+
+    :param X:
+    :return:
+    """
+
+    number_of_examples = X.shape[0]
+
+    sigma = (1 / number_of_examples) * np.dot(X.T, X)
+    U, S, V = svd(sigma)
+
+    return U, S, V
 
 
 '''
@@ -35,3 +53,12 @@ Step 1: Normalize the dataset X
 X_normalized, mu, std = featureNormalize(X)
 print("Values of the first 3 normalized dataset X: \n{}\n".format(X_normalized[:3]))
 
+
+'''
+Step 2: Compute the principal components and the diagonal matrix
+'''
+
+U, S, _ = pca(X_normalized)
+
+print("The principal components: \n{}\n".format(U))
+print("The diagonal matrix: \n{}\n".format(S))
